a94ee8a139ef11ce94f7851ae809c4350bfadb1f
maarcos4g <marcospaullo552@gmail.com>
2022-12-02 11:45:37 -0300

    lesson about lists

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Flutter Demo Home Page'),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.person),
+              trailing: Container(
+                height: 100,
+                child: Icon(Icons.arrow_right_rounded, size: 45),
+              ),
+              title: Text('Marcos'),
+            )
+          ],
+        ));
+  }
+}
